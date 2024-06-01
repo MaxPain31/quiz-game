f507c899e6558e7924b667054eda077cbffed258
MAC LAGA <mac.laga11@gmail.com>
2024-06-01 10:40:33 +0800

    1s

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAdGiP5MflFOTvW9tB0MuwBLXArvag9Myk",
+    authDomain: "projectquizzle.firebaseapp.com",
+    projectId: "projectquizzle",
+    storageBucket: "projectquizzle.appspot.com",
+    messagingSenderId: "138897474466",
+    appId: "1:138897474466:web:83523f4be9fb318eb628fb"
+  }
+};
